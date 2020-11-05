68e9fa7b12ffc86ca7cc61719bb68634965e67c7
anandrajaram21 <anandrajaram21@gmail.com>
2020-11-05 13:28:13 +0530

    added redis to requirements.txt

--- src/web_app/app.py
+++ src/web_app/app.py
@@ -1,37 +1,19 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
+import redis
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "This is just a tiny change": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+app.layout = dbc.Container(
+    dbc.Alert("Hello Bootstrap!", color="success"),
+    className="p-5",
+)
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-server = app.server
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run_server()
